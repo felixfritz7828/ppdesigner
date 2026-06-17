28549220fc490fa856d4a132127fa3ad364e7fe7
felixfritz7828 <161850036+felixfritz7828@users.noreply.github.com>
2026-06-17 15:43:23 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// Sécurité : au chargement, on force la fermeture
+function openPopup(id){
+    document.getElementById(id).style.display = "block";
+}
+
+function closePopup(id){
+    document.getElementById(id).style.display = "none";
+}
